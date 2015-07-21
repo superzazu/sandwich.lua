b0555be8a98a55715ca3eb8183c186a8c372fd2b
SuperZazu <nico.g.allemand@gmail.com>
2015-07-22 00:08:40 +0200

    80-char limit

--- sandwich.lua
+++ sandwich.lua
@@ -3,25 +3,25 @@ local sandwich = {
     _DESCRIPTION  = 'A LÖVE2D resource manager',
     _URL          = 'http://github.com/superzazu/sandwich.lua',
     _LICENSE      = [[
-    Copyright (c) 2014 Nicolas Allemand
+Copyright (c) 2015 Nicolas Allemand
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
     ]]
 }
 
@@ -60,15 +60,17 @@ sandwich.addType = function (type, callback)
 end
 
 sandwich.get = function (type, resource_name, ...)
+    resource_name = prefix .. resource_name
+
     if not resources[type] then
         resources[type] = {}
     end
 
-    if not resources[type][prefix..resource_name] then
-        resources[type][prefix..resource_name] = callbacks[type](prefix..resource_name, ...)
+    if not resources[type][resource_name] then
+        resources[type][resource_name] = callbacks[type](resource_name, ...)
     end
 
-    return resources[type][prefix..resource_name]
+    return resources[type][resource_name]
 end
 
 sandwich.setPrefix = function (p)
